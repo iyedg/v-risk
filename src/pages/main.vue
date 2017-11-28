7dc22062a454ba081449639e9271da9941db8558
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2017-11-28 16:47:08 +0100

    track changes to units and dice in children

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,22 +1,52 @@
-<template>
-  <div id="game">
-    <player position="d"></player>
-    <player position="a"></player>
-    <player position="a"></player>
-    <player position="a"></player>
-    <player position="a"></player>
-    <player position="a"></player>
-  </div>
+<template lang="pug">
+#game
+  player(position="a" @unitsChange="updateUnits" @diceChange="updateDice")
+  player(position="d" @unitsChange="updateUnits" @diceChange="updateDice")
 </template>
 
 <script>
+// For now, the logic is based on having only one attacker
+// and one defender
+// TODO: game logic for multiple named players
+// TODO: track attacked countries
+// TODO: track number of units in each country
+
 import Player from '@/components/player'
 export default {
   name: 'main',
   components: {Player},
   data () {
     return {
+      attackerUnits: null,
+      attackerDice: null,
+      defenderUnits: null,
+      defenderDice: null
+    }
+  },
+  methods: {
+    updateUnits (e) {
+      if (e.position === 'a') {
+        this.attackerUnits = e.units
+      }
+      if (e.position === 'd') {
+        this.defenderUnits = e.units
+      }
+    },
+    updateDice (e) {
+      if (e.position === 'a') {
+        this.attackerDice = e.dice
+      }
+      if (e.position === 'd') {
+        this.defenderDice = e.dice
+      }
     }
   }
 }
 </script>
+
+<style lang="sass" scoped>
+#game
+  display: flex
+  flex-direction: row
+</style>
+
