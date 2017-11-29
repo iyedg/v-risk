2dbfaf2daf26ba5b2bc95d6ee2400df0b3a60325
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2017-11-29 17:14:05 +0100

    seemingly functional attack and blitz functions

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,8 +1,10 @@
 <template lang="pug">
 #game
-  player(class="attacker" position="a" @unitsChange="updateUnits" @diceChange="updateDice")
-  player(class="defender" position="d" @unitsChange="updateUnits" @diceChange="updateDice")
-  button(@click.native.stop="playAttack") Attack
+  player(class="attacker" position="a" @unitsChange="updateUnits" @diceChange="updateDice" :units="attackerUnits")
+  player(class="defender" position="d" @unitsChange="updateUnits" @diceChange="updateDice" :units="defenderUnits")
+  .column
+    button(@click="playAttack") Attack
+    button(@click="blitz") Blitz
 </template>
 
 <script>
@@ -18,10 +20,12 @@ export default {
   components: {Player},
   data () {
     return {
-      attackerUnits: null,
+      attackerUnits: 2,
       attackerDice: null,
-      defenderUnits: null,
-      defenderDice: null
+      attackerThrow: null,
+      defenderUnits: 2,
+      defenderDice: null,
+      defenderThrow: null
     }
   },
   methods: {
@@ -49,6 +53,56 @@ export default {
           (Math.random() * 6) + 1)
         )
       .sort((a, b) => b - a)
+    },
+    playAttack () {
+      // TODO: focus on one programming style, either methods affect properties
+      // or return values
+      // An attacker may not attack with units less than 2
+      // An attacker may not attack a defender with units less than 1
+      if (this.attackerUnits < 2 || this.defenderUnits < 1) {
+        alert('An attacker may not attack with units less than 2 or attack a defender with units les than 1')
+        return
+      }
+      // Each player throws the dice
+      this.attackerThrow = this.rollDice(this.attackerDice)
+      this.defenderThrow = this.rollDice(this.defenderDice)
+      // alert('Attacker: ' + this.attackerThrow + '\n Defender: ' + this.defenderThrow)
+      // A winner is determined
+      // the number of lost units is substracted
+      this.compareThrows().map(v => {
+        if (v === 1) {
+          // number of defender units may not be less than zero
+          if (this.defenderUnits > 0) {
+            this.defenderUnits -= 1
+            this.updateUnits({position: 'd', units: this.defenderUnits})
+          }
+        } else {
+          // number of attacker units may not be less than one
+          if (this.attackerUnits > 1) {
+            this.attackerUnits -= 1
+            this.updateUnits({position: 'a', units: this.attackerUnits})
+          }
+        }
+      })
+    },
+    blitz () {
+      while (this.attackerUnits >= 2 && this.defenderUnits > 0) {
+        this.playAttack()
+      }
+    },
+    compareThrows () {
+      // * The comparison depends on the number of least dice used by the two players
+      // * Reduce the two arrays into an array of 0, 1 where 0
+      // means the attacker lost the attack
+      // * The attacker wins if and only if their die value
+      // is strictly higher than that of the defender and
+      // the defender loses one unit
+      // else, the attacker loses one units
+      if (this.attackerDice >= this.defenderDice) {
+        return this.defenderThrow.map((v, i) => v >= this.attackerThrow[i] ? 0 : 1)
+      } else {
+        return this.attackerThrow.map((v, i) => v > this.defenderThrow[i] ? 1 : 0)
+      }
     }
   }
 }
@@ -69,9 +123,14 @@ html, body, #app, #game
   display: flex
   color: white
   font-family: sans-serif
-  button
+  .column
     order: 2
     background: turquoise
+
+.column
+  display: flex
+  flex-direction: column
+  justify-items: space-around
 
 .attacker
   order: 1
